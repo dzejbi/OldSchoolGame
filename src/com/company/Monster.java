2e385e6d7c1e57c65959400956fd029d7568f8f6
imielin <maciekbojdak@gmail.com>
2015-03-16 15:43:21 +0100

    z walka jak chcesz to marge'uj zmiany głównie w monsterze,
    dodane skile i parametry dla potworków

--- src/com/company/Monster.java
+++ src/com/company/Monster.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Random;
+
 /**
  * Created by Jan on 2015-03-14.
  */
@@ -9,11 +11,17 @@ public class Monster {
 
     private int health = 500;
     private int attack = 20;
+    private int skill_at_attack = 30;
+    private int skill_at_magic = 50;
+    private int lifesuck = 10;
+    private int critical = 60;
     private int movement = 2;
     private int xPosition = 0;
     private int yPosition = 0;
     private boolean alive = true;
-
+    private int action;
+    private Random generator = new Random();
+    
     private int maxXPosition = 10;
     private int minXPosition = 0;
     private int maxYPosition = 10;
@@ -122,6 +130,8 @@ public class Monster {
         this.movement = movement;
         this.name = name;
         charName = name.charAt(0);
+        this.lifesuck = this.attack/2;
+        this.critical = this.attack*3;
         do {
             xPosition = (int) (Math.random() * maxXPosition);
             yPosition = (int) (Math.random() * maxYPosition);
@@ -131,6 +141,62 @@ public class Monster {
     }
 
     private void fight(Monster enemy){
-        enemy.setAlive(false);
+    	System.out.println(this.name+" ambushed "+enemy.name );
+    	while(enemy.alive==true && this.alive==true){
+    		this.action=this.generator.nextInt(2);
+    		enemy.action=this.generator.nextInt(2);
+    		if(this.action==0){
+    			if(this.skill_at_attack<generator.nextInt(100)){
+    				enemy.health-=this.critical;
+    				System.out.println(this.name+" uses slash, it's super effective    "+this.name+" "+this.health+"  "+enemy.name+" "+enemy.health );
+    			}
+    			else{
+    				enemy.health-=this.attack;
+    				System.out.println(this.name+" uses slash                          "+this.name+" "+this.health+"  "+enemy.name+" "+enemy.health );
+    			}
+    		}
+    			else {
+    				if(this.skill_at_magic<generator.nextInt(100)){
+        				enemy.health-=this.attack;
+        				this.health+=this.lifesuck;
+        				System.out.println(this.name+" uses lifesuck, it's super effective    "+this.name+" "+this.health+"  "+enemy.name+" "+enemy.health );
+        			}
+        			else{
+        				enemy.health-=this.lifesuck;
+        				System.out.println(this.name+" uses lifesuck, but it's ineffective    "+this.name+" "+this.health+"  "+enemy.name+" "+enemy.health );
+        			}
+    			}
+    		
+    		if(enemy.action==0){
+    			if(this.skill_at_attack<generator.nextInt(100)){
+    				this.health-=enemy.critical;
+    				System.out.println(enemy.name+" uses slash, it's super effective     "+this.name+" "+this.health+"  "+enemy.name+" "+enemy.health );
+    			}
+    			else{
+    				this.health-=enemy.attack;
+    				System.out.println(enemy.name+" uses slash                           "+this.name+" "+this.health+"  "+enemy.name+" "+enemy.health );
+    			}
+    		}
+    			else {
+    				if(this.skill_at_magic<generator.nextInt(100)){
+    					this.health-=enemy.attack;
+        				enemy.health+=enemy.lifesuck;
+        			System.out.println(enemy.name+" uses lifesuck, it's super effective  "+this.name+" "+this.health+"  "+enemy.name+" "+enemy.health );
+        			}
+        			else{
+        				this.health-=enemy.lifesuck;
+        				System.out.println(enemy.name+" uses lifesuck, but it's ineffective   "+this.name+" "+this.health+"  "+enemy.name+" "+enemy.health );
+        			}
+    			}
+    		
+    		if(enemy.health<=0){
+    			enemy.setAlive(false);
+    			System.out.println("Infamous moster "+enemy.name+" was slain by "+this.name);
+    		}	
+    		else if(this.health<=0){
+    			this.setAlive(false);
+    			System.out.println("Ambush failed. "+this.name+" was slain by "+enemy.name);   	
+    		}
+    	}
     }
 }
