3bf19fa14fb4b1a90873a5e55eca64ee9924c416
imielin <maciekbojdak@gmail.com>
2015-03-16 23:33:11 +0100

    Z graczem
    grac mozna postacie tworzyc i multiplayerowac

--- src/com/company/Hero.java
+++ src/com/company/Hero.java
@@ -1,10 +1,13 @@
 package com.company;
 
+import java.util.Random;
+
 /**
  * Created by Jan on 2015-03-15.
  */
 public class Hero {
 
+	
+	    }
 
 
-}
--- src/com/company/Monster.java
+++ src/com/company/Monster.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  * Created by Jan on 2015-03-14.
@@ -8,8 +9,10 @@ import java.util.Random;
 public class Monster {
 
     public final String TOMBSTONE = "Here Lies a Dead Monster";
-
+    private boolean ifHero=false;
     private int health = 500;
+    private int health_max=500;
+    private int health_regeneration=50;
     private int attack = 20;
     private int skill_at_attack = 30;
     private int skill_at_magic = 50;
@@ -21,7 +24,7 @@ public class Monster {
     private boolean alive = true;
     private int action;
     private Random generator = new Random();
-    
+    private int speciality = 50;
     private int maxXPosition = 10;
     private int minXPosition = 0;
     private int maxYPosition = 10;
@@ -86,6 +89,8 @@ public class Monster {
     }
 
     public void search(){
+    	if(health_max>health)
+    		health+=health_regeneration;
         if(alive) {
             int xMonster = -1, yMonster = -1;
 
@@ -124,13 +129,25 @@ public class Monster {
         }
     }
 
-    public Monster(int attack, int health, int movement, String name) {
+    public Monster(int attack, int health, int movement, String name, int skill_at_attack,int skill_at_magic,int speciality, boolean Hero) {
         this.attack = attack;
         this.health = health;
+        this.health_max=health;
+        this.health_regeneration=health/10;
         this.movement = movement;
         this.name = name;
+        this.skill_at_attack=skill_at_attack;
+        this.skill_at_magic=skill_at_magic;
+        this.ifHero=Hero;
+        if(speciality>70)
+        	this.speciality=70;
+        else if(speciality<30){
+        	this.speciality=30;
+        }
+        	else
+        		this.speciality=speciality;
         charName = name.charAt(0);
-        this.lifesuck = this.attack/2;
+        this.lifesuck = this.skill_at_magic/2;
         this.critical = this.attack*3;
         do {
             xPosition = (int) (Math.random() * maxXPosition);
@@ -139,19 +156,46 @@ public class Monster {
         while (myBoard.battleBoard[xPosition][yPosition] != '*');
         myBoard.battleBoard[xPosition][yPosition] = charName;
     }
-
+    public Monster(int attack, int health, int movement, String name) {
+        this.attack = attack;
+        this.health = health;
+        this.health_max=health;
+        this.health_regeneration=health/10;
+        this.movement = movement;
+        this.name = name;
+        charName = name.charAt(0);
+        this.lifesuck = this.skill_at_magic/2;
+        this.critical = this.attack*3;
+        do {
+            xPosition = (int) (Math.random() * maxXPosition);
+            yPosition = (int) (Math.random() * maxYPosition);
+        }
+        while (myBoard.battleBoard[xPosition][yPosition] != '*');
+        myBoard.battleBoard[xPosition][yPosition] = charName;
+    }
     private void fight(Monster enemy){
+    	int demagecountenemy=0, demagecountattacker=0;
+    	int spellcountenemy=0, spellcountattacker=0;
+    	int succesrateenemy=0, succesrateattacker=0;
+    	int successpellenemy=0, successpellattacker=0;
+    	int tryrateenemy=1, tryrateattacker=1;
+    	int tryspellenemy=1, tryspellattacker=1;
     	System.out.println(this.name+" ambushed "+enemy.name );
     	while(enemy.alive==true && this.alive==true){
-    		this.action=this.generator.nextInt(2);
-    		enemy.action=this.generator.nextInt(2);
-    		if(this.action==0){
+    		this.action=this.generator.nextInt(100);
+    		enemy.action=this.generator.nextInt(100);
+    		if(this.action<speciality){
     			if(this.skill_at_attack<generator.nextInt(100)){
     				enemy.health-=this.critical;
+    				demagecountattacker+=this.critical;
+    				tryrateattacker++;
+    				succesrateattacker++;
     				System.out.println(this.name+" uses slash, it's super effective    "+this.name+" "+this.health+"  "+enemy.name+" "+enemy.health );
     			}
     			else{
     				enemy.health-=this.attack;
+    				demagecountattacker+=this.attack;
+    				tryrateattacker++;
     				System.out.println(this.name+" uses slash                          "+this.name+" "+this.health+"  "+enemy.name+" "+enemy.health );
     			}
     		}
@@ -159,10 +203,15 @@ public class Monster {
     				if(this.skill_at_magic<generator.nextInt(100)){
         				enemy.health-=this.attack;
         				this.health+=this.lifesuck;
+        				spellcountattacker=this.attack+this.lifesuck;
+        				tryspellattacker++;
+        				successpellattacker++;
         				System.out.println(this.name+" uses lifesuck, it's super effective    "+this.name+" "+this.health+"  "+enemy.name+" "+enemy.health );
         			}
         			else{
         				enemy.health-=this.lifesuck;
+        				spellcountattacker+=this.lifesuck;
+        				tryspellattacker++;
         				System.out.println(this.name+" uses lifesuck, but it's ineffective    "+this.name+" "+this.health+"  "+enemy.name+" "+enemy.health );
         			}
     			}
@@ -170,10 +219,15 @@ public class Monster {
     		if(enemy.action==0){
     			if(this.skill_at_attack<generator.nextInt(100)){
     				this.health-=enemy.critical;
+    				demagecountenemy+=enemy.critical;
+    				tryrateenemy++;
+    				succesrateenemy++;
     				System.out.println(enemy.name+" uses slash, it's super effective     "+this.name+" "+this.health+"  "+enemy.name+" "+enemy.health );
     			}
     			else{
     				this.health-=enemy.attack;
+    				demagecountenemy+=enemy.attack;
+    				tryrateenemy++;
     				System.out.println(enemy.name+" uses slash                           "+this.name+" "+this.health+"  "+enemy.name+" "+enemy.health );
     			}
     		}
@@ -181,10 +235,15 @@ public class Monster {
     				if(this.skill_at_magic<generator.nextInt(100)){
     					this.health-=enemy.attack;
         				enemy.health+=enemy.lifesuck;
+        				spellcountenemy=enemy.attack+enemy.lifesuck;
+        				tryspellenemy++;
+        				successpellenemy++;
         			System.out.println(enemy.name+" uses lifesuck, it's super effective  "+this.name+" "+this.health+"  "+enemy.name+" "+enemy.health );
         			}
         			else{
         				this.health-=enemy.lifesuck;
+        				spellcountenemy=enemy.lifesuck;
+        				tryspellenemy++;
         				System.out.println(enemy.name+" uses lifesuck, but it's ineffective   "+this.name+" "+this.health+"  "+enemy.name+" "+enemy.health );
         			}
     			}
@@ -192,10 +251,115 @@ public class Monster {
     		if(enemy.health<=0){
     			enemy.setAlive(false);
     			System.out.println("Infamous moster "+enemy.name+" was slain by "+this.name);
-    		}	
+    			if(this.ifHero==false){
+    			System.out.println("Infamous moster "+enemy.name+" was slain by "+this.name);
+    			if((demagecountattacker*succesrateattacker^2/tryrateattacker)>(spellcountattacker*successpellattacker^2/tryspellattacker)){
+    				this.speciality+=5;
+    				this.attack+=0.1*this.attack;
+    				this.skill_at_attack+=5;
+    				System.out.println(this.name+" took a level in Badass");
+    			}
+    			else{
+    				this.speciality-=5;
+    				this.lifesuck+=0.2*this.lifesuck;
+    				this.skill_at_magic+=5;
+    				System.out.println(this.name+" took a level in Wizardry");
+    			}
+    			}
+    			else{
+    				System.out.println(this.name+" have gained expirience. You can use it to upgrade your skills and health. How would you like to spend it?");
+    				System.out.println("1 - Health    2 - Attack   3 - Magic   4 - Attack skill   5 - Magic skill   6 - More magic based fighting style   7 - More melee based fightin style");
+
+    				int exp=2;
+    				while(exp!=0){
+    					int n= new Scanner(System.in).nextInt();
+    					switch (n) {
+    		            case 1:  this.health_max+=10;
+    		            		 exp--;
+    		            		 break;
+    		            case 2:  this.attack+=5;
+    		        			 exp--;
+    		                     break;
+    		            case 3:  this.lifesuck+=3;
+    		            		 exp--;
+    		                     break;
+    		            case 4:  this.skill_at_attack+=7;
+    		            		 exp--;
+    		                     break;
+    		            case 5:  this.skill_at_magic+=10;
+    		            		 exp--;
+    		                     break;
+    		            case 6:  this.speciality-=5;
+    		            		 exp--;
+    		                     break;
+    		            case 7:  this.speciality+=5;
+    		            		 exp--;
+    		                     break;
+    		            
+    		            default: System.out.println("Ivalid instruction");
+    		                     break;
+    		        }
+
+    				}
+    				
+    			}
+    		}
     		else if(this.health<=0){
     			this.setAlive(false);
-    			System.out.println("Ambush failed. "+this.name+" was slain by "+enemy.name);   	
+    			System.out.println("Ambush failed. "+this.name+" was slain by "+enemy.name);   
+    			if(enemy.ifHero==false){
+    			if((demagecountenemy*succesrateenemy^2/tryrateenemy)>(spellcountenemy*successpellenemy^2/tryspellenemy)){
+    				enemy.speciality+=5;
+    				enemy.attack+=0.1*this.attack;
+    				enemy.skill_at_attack+=5;
+    				System.out.println(enemy.name+" took a level in Badass");
+    			}
+    			else{
+    				enemy.speciality-=5;
+    				enemy.lifesuck+=0.2*this.lifesuck;
+    				enemy.skill_at_magic+=5;
+    				System.out.println(enemy.name+" took a level in Wizardry");
+    			}
+    			}
+    			else{
+    				System.out.println(enemy.name+" have gained expirience. You can use it to upgrade your skills and health. How would you like to spend it?");
+    				System.out.println("1 - Health    2 - Attack   3 - Magic   4 - Attack skill   5 - Magic skill   6 - More magic based fighting style   7 - More melee based fightin style");
+
+    				int exp=2;
+    				while(exp!=0){
+    					int n= new Scanner(System.in).nextInt();
+    					switch (n) {
+    		            case 1:  enemy.health_max+=10;
+    		            		 exp--;
+    		            		 break;
+    		            case 2:  enemy.attack+=5;
+    		        			 exp--;
+    		                     break;
+    		            case 3:  enemy.lifesuck+=3;
+    		            		 exp--;
+    		                     break;
+    		            case 4:  enemy.skill_at_attack+=7;
+    		            		 exp--;
+    		                     break;
+    		            case 5:  enemy.skill_at_magic+=10;
+    		            		 exp--;
+    		                     break;
+    		            case 6:  enemy.speciality-=5;
+    		            		 exp--;
+    		                     break;
+    		            case 7:  enemy.speciality+=5;
+    		            		 exp--;
+    		                     break;
+    		            
+    		            default: System.out.println("Ivalid instruction");
+    		                     break;
+    		        }
+
+    				}
+    				
+    			}
+    				
+    				
     		}
     	}
     }
